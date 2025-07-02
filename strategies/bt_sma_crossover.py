39f7ca4d106bdcb081a8e9c66b0c0df4ec0471d1
jiaweiwang04 <chloewang20041111@outlook.com>
2025-07-02 04:22:34 +0100

    second day stategies which is sma crossover

---
+++ strategies/bt_sma_crossover.py
@@ -0,0 +1,14 @@
+import backtrader as bt
+
+class SmaCrossover(bt.Strategy):
+    def __init__(self):
+        self.sma10 = bt.indicators.MovingAverageSimple(self.data.close, period=10 )
+        self.sma30 = bt.indicators.MovingAverageSimple(self.data.close, period=30 )
+
+    def __next__(self):
+        if self.sma10[0] > self.sma30[0] and self.sma10[-1] < self.sma30[-1]:
+            if not self.position:
+                self.buy()
+        elif self.sma10[0] < self.sma30[0] and self.sma10[-1] < self.sma30[-1]:
+            if self.position:
+                self.close()
