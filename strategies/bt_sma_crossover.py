fad5607e56778ac69e75df3d90cb3b944359d9ff
jiaweiwang04 <chloewang20041111@outlook.com>
2025-07-04 10:59:37 +0800

    small change in sma crossover strategy

--- strategies/bt_sma_crossover.py
+++ strategies/bt_sma_crossover.py
@@ -1,9 +1,10 @@
 import backtrader as bt
 
 class SmaCrossover(bt.Strategy):
+    params = dict(pfast = 10, pslow = 30)
     def __init__(self):
-        self.sma10 = bt.indicators.MovingAverageSimple(self.data.close, period=10 )
-        self.sma30 = bt.indicators.MovingAverageSimple(self.data.close, period=30 )
+        self.sma10 = bt.indicators.MovingAverageSimple(self.data.close, period=self.p.pfast )
+        self.sma30 = bt.indicators.MovingAverageSimple(self.data.close, period=self.p.pslow )
 
     def next(self):
         if self.sma10[0] > self.sma30[0] and self.sma10[-1] < self.sma30[-1]:
